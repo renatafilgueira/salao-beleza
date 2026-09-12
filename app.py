9a21212fad20f22a72bec4e5fdd141e86326e71c
Renata <renatalorrayne29@gmail.com>
2026-09-12 00:38:56 -0300

    Atualiza estrutura e estilos do Bella Rosa

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def inicio():
-    return render_template("index.html")
-
+    return send_from_directory(".", "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
